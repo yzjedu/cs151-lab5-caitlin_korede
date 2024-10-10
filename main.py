ba51c3c93ea88e482a3fdcaef7405741348518e7
oaoni <oaoni@loyola.edu>
2024-10-10 19:20:44 -0400

    Added the beginning of the program

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-# Read the README.md
-# Read it again
-# Your code here
-# Delete these 4 lines of comments
+# Create variables answer and SENTINEL for later use
+# answer stores withdraw and deposit input values
+# SENTINEL used for if user wishes to exit the program
+answer = ""
+SENTINEL = "E"
+
+print("Welcome to the Automatic Teller Machine. What would you like to do?")
+print("For Deposit, press D")
+print("For Withdraw, press W")
+print("To View Balance, press V")
+print("If you wish to Exit, press E")
+choice = str(input("What would you like to do?: "))
+
+while choice.upper() != SENTINEL:
+    choice = input("This is just a placeholder input. ")
+
+print("Thank you for using our ATM.")
